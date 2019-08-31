1a2b22fd95a78d5bff5d2bf320e2ad34f38f4c5d
Carlos Machado <carlosedumachado@edu.univali.br>
2019-08-31 19:34:10 -0300

    add mouse no menu

--- Fake Perguntados/src/com/bakerystudios/gui/menu/MainMenu.java
+++ Fake Perguntados/src/com/bakerystudios/gui/menu/MainMenu.java
@@ -1,5 +1,10 @@
 package com.bakerystudios.gui.menu;
 
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.bakerystudios.game.Game;
 import com.bakerystudios.game.GameState;
 import com.bakerystudios.gui.menu.core.Menu;
 import com.bakerystudios.gui.menu.core.MenuOption;
@@ -7,28 +12,114 @@ import com.bakerystudios.gui.menu.core.MenuState;
 
 public class MainMenu extends Menu {
 
+	private List<Integer> coordenadasX = new ArrayList<>();
+	private List<Integer> coordenadasY = new ArrayList<>();
+
+	private int mx = 0, my = 0;
+	private boolean clicked = false;
+	
+	int currentFrame = 0, maxFrame = 30;
+
 	public MainMenu() {
 		option.addOption(new MenuOption("NOVO JOGO"));
 		option.addOption(new MenuOption("SCORES"));
 		option.addOption(new MenuOption("OPÇÕES"));
 		option.addOption(new MenuOption("SAIR"));
 		option.firstOption();
+
+		// Novo Jogo Coordenadas
+		coordenadasX.add(266);
+		coordenadasX.add(533);
+		coordenadasY.add(219);
+		coordenadasY.add(268);
+		
+		// Scores Coordenadas
+		coordenadasX.add(306);
+		coordenadasX.add(494);
+		coordenadasY.add(288);
+		coordenadasY.add(337);
+		
+		// OPÇÕES Coordenadas
+		coordenadasX.add(306);
+		coordenadasX.add(494);
+		coordenadasY.add(359);
+		coordenadasY.add(408);
+		
+		// SAIR Coordenadas
+		coordenadasX.add(358);
+		coordenadasX.add(451);
+		coordenadasY.add(429);
+		coordenadasY.add(477);
+	}
+
+	public void update() {
+		if (up) {
+			Game.audio.playSelect();
+			up = false;
+			option.previousOption();
+		}
+		if (down) {
+			Game.audio.playSelect();
+			down = false;
+			option.nextOption();
+		}
+		if (enter) {
+			Game.audio.playSelect();
+			enter = false;
+			executeOption();
+			option.firstOption();
+		}
+		
+		currentFrame++;
+		int j = 0;
+		for (int i = 0; i < coordenadasX.size(); i += 2, j++) 
+			if ((mx > coordenadasX.get(i) && mx < coordenadasX.get(i + 1)) 
+					&& (my > coordenadasY.get(i) && my < coordenadasY.get(i + 1))) {
+				option.setCurOption(option.getOption(j));
+				if(clicked) {
+					clicked = false;
+					if(currentFrame > maxFrame) {
+						enter = true;
+						currentFrame = 0;
+					}	
+				}
+			}
 	}
 
 	@Override
 	public void executeOption() {
-		if(option.getCurOption() == option.getOption(0)) {
+		if (option.getCurOption() == option.getOption(0)) {
 			// NEW GAME
 			GameState.state = GameState.PLAYING;
-		} else if(option.getCurOption() == option.getOption(1)) {
+		} else if (option.getCurOption() == option.getOption(1)) {
 			// LOAD
-		} else if(option.getCurOption() == option.getOption(2)) {
+		} else if (option.getCurOption() == option.getOption(2)) {
 			// OPTIONS
 			MenuState.state = MenuState.OPTION;
-		} else if(option.getCurOption() == option.getOption(3)) {
+		} else if (option.getCurOption() == option.getOption(3)) {
 			// EXIT
 			System.exit(0);
 		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		clicked = true;
+	}
+	
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		clicked = false;
+	}
+	
+	@Override
+	public void mouseDragged(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		mx = e.getX();
+		my = e.getY();
 	}
 
 }
--- Fake Perguntados/src/com/bakerystudios/gui/menu/core/Menu.java
+++ Fake Perguntados/src/com/bakerystudios/gui/menu/core/Menu.java
@@ -6,6 +6,8 @@ import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
+import javax.swing.JLabel;
+
 import com.bakerystudios.game.Game;
 import com.bakerystudios.game.GameState;
 import com.bakerystudios.game.Screen;
@@ -18,43 +20,43 @@ import com.bakerystudios.tools.Updateble;
 public abstract class Menu extends InputListener implements Renderable, Updateble {
 
 	protected OptionManager option = new OptionManager(4);
-	
+
 	protected static boolean up, down, enter;
-	
+
 	protected boolean pause = false;
-	
+
 	private Sprite background;
-	
+
 	public Menu() {
 		background = new Sprite("/sprites/background01.jpg");
 	}
-	
+
 	public abstract void executeOption();
 
 	@Override
 	public void update() {
-		if(up) {
+		if (up) {
 			Game.audio.playSelect();
 			up = false;
 			option.previousOption();
 		}
-		if(down) {
+		if (down) {
 			Game.audio.playSelect();
 			down = false;
 			option.nextOption();
 		}
-		if(enter) {
+		if (enter) {
 			Game.audio.playSelect();
 			enter = false;
 			executeOption();
 			option.firstOption();
 		}
 	}
-	
+
 	protected void drawCentralizedString(Graphics g, String str, int y) {
 		g.drawString(str, Screen.WIDTH / 2 - g.getFontMetrics().stringWidth(str) / 2, y);
 	}
-	
+
 	protected void fillCentralizedRect(Graphics g, int y, int width, int height) {
 		g.fillRect(Screen.WIDTH / 2 - width / 2, y, width, height);
 	}
@@ -63,95 +65,126 @@ public abstract class Menu extends InputListener implements Renderable, Updatebl
 	public void render(Graphics g) {
 		int y = (int) (Screen.WIDTH * 0.16);
 		g.drawImage(background.getSprite(), 0, 0, null);
-		
+
 		g.setColor(Color.WHITE);
 		g.setFont(new Font("arial", Font.BOLD, (int) (Screen.WIDTH * 0.07)));
-		
 
 		// GAME TITLE ----------------------------------------------------------
 		drawCentralizedString(g, "FAKE PERGUNTADOS", y);
 		y += g.getFontMetrics().getHeight() * 2.0;
-		
+
 		// MENU OPTIONS ----------------------------------------------------------
 		g.setColor(Color.WHITE);
 		g.setFont(new Font("arial", Font.BOLD, (int) (Screen.WIDTH * 0.05)));
-		
-		for(int i = 0; i < option.optionAmount(); i++) {
+
+		for (int i = 0; i < option.optionAmount(); i++) {
 			String name = option.getOption(i).getName();
 			int fontHeight = g.getFontMetrics().getHeight();
-			if(option.getCurOption() == option.getOption(i)) {
+			if (option.getCurOption() == option.getOption(i)) {
 				int fontWidth = g.getFontMetrics().stringWidth(name);
 				g.setColor(Color.GRAY);
-				fillCentralizedRect(g, y - (int) (fontHeight * 0.85), (int) (fontWidth * 1.1), (int) (fontHeight * 1.1));
+				fillCentralizedRect(g, y - (int) (fontHeight * 0.85), (int) (fontWidth * 1.1),
+						(int) (fontHeight * 1.1));
 			}
 			g.setColor(Color.WHITE);
 			drawCentralizedString(g, name, y);
 			y += fontHeight * 1.5;
 		}
-		
-		
-		
 	}
-	
+
 	@Override
 	public void keyPressed(KeyEvent e) {
-		
-		if(e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W) {
-			if(GameState.state == GameState.MENU) {
+
+		if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W) {
+			if (GameState.state == GameState.MENU) {
 				Menu.setUp(true);
 			}
-		} else if(e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
-			if(GameState.state == GameState.MENU) {
+		} else if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
+			if (GameState.state == GameState.MENU) {
 				Menu.setDown(true);
 			}
 		}
-		
-		if(e.getKeyCode() == KeyEvent.VK_ENTER) {
-			if(GameState.state == GameState.MENU) {
+
+		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+			if (GameState.state == GameState.MENU) {
 				Menu.setEnter(true);
 			}
 		}
-		
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		
-		if(e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W) {
-			if(GameState.state == GameState.MENU) {
+
+		if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W) {
+			if (GameState.state == GameState.MENU) {
 				Menu.setUp(false);
 			}
-		} else if(e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
-			if(GameState.state == GameState.MENU) {
+		} else if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
+			if (GameState.state == GameState.MENU) {
 				Menu.setDown(false);
 			}
 		}
-		
-		if(e.getKeyCode() == KeyEvent.VK_ENTER) {
-			if(GameState.state == GameState.MENU) {
+
+		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+			if (GameState.state == GameState.MENU) {
 				Menu.setEnter(false);
 			}
 		}
 	}
 
 	@Override
-	public void keyTyped(KeyEvent e) {}
+	public void keyTyped(KeyEvent e) {
+
+	}
+
 	@Override
-	public void mouseClicked(MouseEvent e) {}
+	public void mouseClicked(MouseEvent e) {
+
+	}
+
 	@Override
-	public void mouseEntered(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {
+
+	}
+
 	@Override
-	public void mouseExited(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {
+
+	}
+
 	@Override
-	public void mousePressed(MouseEvent e) {}
+	public void mousePressed(MouseEvent e) {
+
+	}
+
 	@Override
-	public void mouseReleased(MouseEvent e) {}
-	
-	public static boolean isUp() {return up;}
-	public static void setUp(boolean up) {Menu.up = up;}
-	public static boolean isDown() {return down;}
-	public static void setDown(boolean down) {Menu.down = down;}
-	public static boolean isEnter() {return enter;}
-	public static void setEnter(boolean enter) {Menu.enter = enter;}
-	
+	public void mouseReleased(MouseEvent e) {
+
+	}
+
+	public static boolean isUp() {
+		return up;
+	}
+
+	public static void setUp(boolean up) {
+		Menu.up = up;
+	}
+
+	public static boolean isDown() {
+		return down;
+	}
+
+	public static void setDown(boolean down) {
+		Menu.down = down;
+	}
+
+	public static boolean isEnter() {
+		return enter;
+	}
+
+	public static void setEnter(boolean enter) {
+		Menu.enter = enter;
+	}
+
 }
